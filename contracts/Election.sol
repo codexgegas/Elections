255d7fe4ad2df193184aad28c59569e753489240
Vikram <51349461+codexgegas@users.noreply.github.com>
2022-05-09 23:14:21 +0530

    Update Election.sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,39 +1,38 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.5.16;
 
 contract Election {
-    // Model a Candidate
-    struct Candidate {
-        uint id;
-        string name;
-        uint voteCount;
-    }
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	// Store accounts that have voted
+	mapping(address => bool) public voters;
+	// Store a Candidate
+	// Fetch Candidate
+	mapping(uint => Candidate) public candidates;
+	// Store Candidates Count
+	uint public candidatesCount;
 
-    // Store accounts that have voted
-    mapping(address => bool) public voters;
-    // Store Candidates
-    // Fetch Candidate
-    mapping(uint => Candidate) public candidates;
-    // Store Candidates Count
-    uint public candidatesCount;
-
-    // voted event
-    event votedEvent (
+	event votedEvent (
         uint indexed _candidateId
     );
+	
+//Election 
+	constructor() public {
+		addCandidates("Candidate 1");
+		addCandidates("Candidate 2");
+	}
+//addCandidates
+	function addCandidates (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
 
-    constructor ()  {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
-    }
-
-    function addCandidate (string  memory _name) private {
-        candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-    }
-
-    function vote (uint _candidateId) public {
+	function vote (uint _candidateId) public {
         // require that they haven't voted before
         require(!voters[msg.sender]);
 
@@ -46,7 +45,8 @@ contract Election {
         // update candidate vote Count
         candidates[_candidateId].voteCount ++;
 
-        // trigger voted event
         emit votedEvent(_candidateId);
     }
+
+
 }
